c0b36c25e2a1fb45f0bb8b7d222a0e2289cb9a31
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-10-29 16:43:33 +0800

    Add files via upload

---
+++ IT111L_Game (latest)/IT111L_Game/IT111L_Game/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using MiniGameCardMemory;
+
+
+namespace IT111L_Game
+{
+    internal class Program
+    {
+
+        static public int level = 1;
+        static void Main(string[] args)
+        {
+            MiniGameCardMemory.CardGameMain miniGameCard = new MiniGameCardMemory.CardGameMain(level);
+            miniGameCard.CardGameMainDisplay();
+
+            Console.WriteLine("The program is still running");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IT111L_Game (latest)/IT111L_Game/MiniGameCardMemory/CardGameForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MiniGameCardMemory
+{
+    internal class CardGameForm : Form
+    {
+
+        public static int currentPlayerLevel;
+
+        MiniGameMainTimer timer = new MiniGameMainTimer();
+        public CardGameElements elements;
+
+        public CardGameForm(int level)
+        {
+            currentPlayerLevel = level;
+
+            this.Name = "MiniGameCardGame";
+            this.Size = new Size(800, 500);
+            this.DoubleBuffered = true;
+            this.Text = "Pixel Quest Mini Game: Card Memory Game";
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.BackgroundImage = MiniGameCardResources.minigame_bg;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.ControlBox = false;
+
+            AddFormElements();
+        }
+
+        public void AddFormElements()
+        {
+            CardGameElements elements = new CardGameElements(this);
+
+            this.Controls.Add(elements.CardMiniTitle);
+            this.Controls.Add(timer.time);
+
+            elements.LoadPictures();
+        }
+
+
+        private static CardGameForm instance;
+        public static CardGameForm Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CardGameForm(3);
+                }
+                return instance;
+            }
+        }
+
+        public void CloseForm()
+        {
+            Application.ExitThread();
+        }
+
+    }
+}
