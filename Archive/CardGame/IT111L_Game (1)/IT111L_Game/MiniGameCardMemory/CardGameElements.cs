7d0470f417a969a002f37e637c4e0163be231429
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-10-30 09:47:16 +0800

    Add files via upload

---
+++ Archive/CardGame/IT111L_Game (1)/IT111L_Game/MiniGameCardMemory/CardGameElements.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using FontGameCollection;
+
+
+namespace MiniGameCardMemory
+{
+    internal class CardGameElements : CardGameInfo
+    {
+
+        public static FontGameCollection.FontGame fontGame = new FontGameCollection.FontGame();
+
+        public CardGameFormLogic CGFLogic = new CardGameFormLogic();
+
+
+        private CardGameForm cardGForm;
+        public CardGameElements(CardGameForm form)
+        {
+            cardGForm = form;
+        }
+
+
+        private Label cardMiniTitle = new Label
+        {
+            Text = "Mini Game Trap: Card Wars",
+            Size = new Size(800, 50),
+            Font = new Font(fontGame.pfc.Families[1], 28),
+            Location = new Point(0, 0)
+        };
+
+        public Label CardMiniTitle
+        {
+            get { return cardMiniTitle; }
+            set { cardMiniTitle = null; }
+        }
+
+
+        public List<int> cardIntCollection = new List<int>();
+        
+        public void LoadPictures()
+        {
+            
+            currGameLevel = CardGameForm.currentPlayerLevel;
+            Console.WriteLine(currGameLevel);
+            int countRow = 0;
+
+            switch (currGameLevel)
+            {
+                case 1:
+                    numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4 };
+                    break;
+                case 2:
+                    numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 };
+                    break;
+                case 3:
+                    numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+                    break;
+            }
+
+            var randomList = numbers.OrderBy(x => Guid.NewGuid()).ToList();
+            cardIntCollection = randomList;
+
+            int leftPosition = 250;
+            int topPosition = 100;
+            int rows = 0;
+
+            switch (currGameLevel)
+            {
+                case 1:
+                    countRow = 4;
+                    break;
+                case 2:
+                    countRow = 5;
+                    break;
+                case 3:
+                    countRow = 6;
+                    break;
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int tempI = i;
+                if (tempI >= numbers.Count / 2)
+                {
+                    switch (currGameLevel)
+                    {
+                        case 1:
+                            switch (tempI)
+                            {
+                                case 4:
+                                    tempI = 0;
+                                    break;
+                                case 5:
+                                    tempI = 1;
+                                    break;
+                                case 6:
+                                    tempI = 2;
+                                    break;
+                                case 7:
+                                    tempI = 3;
+                                    break;
+                                case 8:
+                                    tempI = 4;
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            switch (tempI)
+                            {
+                                case 5:
+                                    tempI = 0;
+                                    break;
+                                case 6:
+                                    tempI = 1;
+                                    break;
+                                case 7:
+                                    tempI = 2;
+                                    break;
+                                case 8:
+                                    tempI = 3;
+                                    break;
+                                case 9:
+                                    tempI = 4;
+                                    break;
+                                case 10:
+                                    tempI = 5;
+                                    break;
+                            }
+                            break;
+                        case 3:
+                            switch (tempI)
+                            {
+                                case 6:
+                                    tempI = 0;
+                                    break;
+                                case 7:
+                                    tempI = 1;
+                                    break;
+                                case 8:
+                                    tempI = 2;
+                                    break;
+                                case 9:
+                                    tempI = 3;
+                                    break;
+                                case 10:
+                                    tempI = 4;
+                                    break;
+                                case 11:
+                                    tempI = 5;
+                                    break;
+                            }
+                            break;
+                    }
+                }
+
+
+                PictureBox currPB = new PictureBox
+                {
+                    Height = 80,
+                    Width = 50,
+                    BackColor = Color.White,
+                    Tag = (tempI + 1).ToString(),
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+
+                };
+                currPB.Click += CGFLogic.CardClicked;
+                cardPicturesCollection.Add(currPB);
+                cardPicturesCollection[i].Tag = cardIntCollection[i].ToString();
+
+                if (rows < countRow)
+                {
+                    rows++;
+                    currPB.Left = leftPosition;
+                    currPB.Top = topPosition;
+                    cardGForm.Controls.Add(currPB);
+                    leftPosition = leftPosition + 60;
+                }
+                if (rows == countRow)
+                {
+                    leftPosition = 250;
+                    topPosition += 90;
+                    rows = 0;
+                }
+            }
+
+            CGFLogic.NewGameOrderCards();
+        }
+
+
+    }
+}
+
---
+++ Archive/CardGame/IT111L_Game (1)/IT111L_Game/MiniGameCardMemory/MiniGameMainTimer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace MiniGameCardMemory
+{
+    internal class MiniGameMainTimer : CardGameInfo
+    {
+        public System.Windows.Forms.Timer miniGameTick = new System.Windows.Forms.Timer();
+        public Label time = LblTimer;
+
+        public void CGFormTimerStart()
+        {
+            miniGameTick.Interval = 1000;
+            miniGameTick.Enabled = true;
+            miniGameTick.Tick += GameActions;
+            miniGameTick.Start();
+        }
+
+        public void CGFormTimerStop(string message)
+        {
+            gameOver = true;
+            miniGameTick.Stop();
+            MessageBox.Show(message);
+
+            CardGameForm.Instance.CloseForm();            
+        }
+
+        public void GameFormTimer()
+        {
+            totalTime--;
+            LblTimer.Text = $"Time Left: {totalTime}";
+        }
+
+        public void GameActions(object sender, EventArgs e)
+        {
+            GameFormTimer();
+
+            if (totalTime == 0)
+            {
+                CGFormTimerStop("Loser");
+
+                foreach (PictureBox x in cardPicturesCollection)
+                {
+                    if (x.Tag != null)
+                    {
+                        x.Image = (Image)MiniGameCardResources.ResourceManager.GetObject("_" + x.Tag.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
