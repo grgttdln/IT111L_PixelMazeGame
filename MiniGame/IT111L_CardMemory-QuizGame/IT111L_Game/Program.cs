f933b2e8d6e54d04d0ec7eab4902a66742876ad5
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-01 21:38:04 +0800

    Add files via upload

---
+++ MiniGame/IT111L_CardMemory-QuizGame/IT111L_Game/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using MiniGameCardMemory;
+using MiniGameLogicQuiz;
+using MiniGameWindow;
+
+namespace IT111L_Game
+{
+    internal class Program
+    {
+
+        static public int level = 1;
+        static void Main(string[] args)
+        {
+            MiniGameLogicQuiz.QuizGameMain miniGameQuiz = new MiniGameLogicQuiz.QuizGameMain(level);
+            miniGameQuiz.MiniGameMainDisplay();
+           
+
+            /*
+            // Mini Game Card
+            MiniGameCardMemory.CardGameMain miniGameCard = new MiniGameCardMemory.CardGameMain(level);
+            miniGameCard.MiniGameMainDisplay();
+            */
+
+            Console.WriteLine("The program is still running");
+            Console.ReadLine();
+        }
+    }
+}
