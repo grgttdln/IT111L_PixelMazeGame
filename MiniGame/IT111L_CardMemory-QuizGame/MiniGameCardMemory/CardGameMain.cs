eb121571ce707915a957185297d42e832f3bdf02
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-01 21:39:44 +0800

    Add files via upload

---
+++ MiniGame/IT111L_CardMemory-QuizGame/MiniGameCardMemory/CardGameMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Reflection.Emit;
+using MiniGameWindow;
+
+namespace MiniGameCardMemory
+{
+
+    public class CardGameMain : MiniGameWindow.MiniGameForm
+    {
+        public CardGameMain(int level) : base(level)
+        {
+            
+        }
+
+        public override void MiniGameMainDisplay()
+        {
+            CardGameForm cardGame = new CardGameForm(Level);
+            Application.Run(cardGame);
+        }
+    }
+}
