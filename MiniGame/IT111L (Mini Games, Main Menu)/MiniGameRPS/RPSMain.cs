78cc73ac8626e7d051a6e657bc578968912e6a19
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-04 19:50:34 +0800

    Add files via upload

---
+++ MiniGame/IT111L (Mini Games, Main Menu)/MiniGameRPS/RPSMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiniGameWindow;
+
+namespace MiniGameRPS
+{
+    public class RPSMain : MiniGameWindow.MiniGameForm
+    {
+        public RPSMain (int level) : base(level)
+        {
+
+        }
+
+        public override void MiniGameMainDisplay()
+        {
+            RPSForm rpsGame = new RPSForm(Level);
+            Application.Run(rpsGame);
+            IsWin = RPSLogic.isWin;
+        }
+
+
+    }
+}
