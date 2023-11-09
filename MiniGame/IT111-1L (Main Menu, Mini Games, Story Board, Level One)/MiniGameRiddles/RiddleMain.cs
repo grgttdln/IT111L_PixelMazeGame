879954977f1da95345d8f728ce083b19233aa3f3
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-09 08:26:40 +0800

    Add files via upload

---
+++ MiniGame/IT111-1L (Main Menu, Mini Games, Story Board, Level One)/MiniGameRiddles/RiddleMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiniGameWindow;
+
+namespace MiniGameRiddles
+{
+    public class RiddleMain : MiniGameWindow.MiniGameForm
+    {
+        public RiddleMain(int level) : base(level)
+        {
+
+        }
+
+        public override void MiniGameMainDisplay()
+        {
+            RiddleForm riddleGame = new RiddleForm(Level);
+            Application.Run(riddleGame);
+            IsWin = TimerInfo.isWin;
+        }
+
+
+    }
+}
