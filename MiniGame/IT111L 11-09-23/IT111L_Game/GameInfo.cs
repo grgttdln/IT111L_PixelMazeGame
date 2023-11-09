a9189e2e04c96c4da5361bc43aa9f0aaddae9b68
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-09 17:48:53 +0800

    Add files via upload

---
+++ MiniGame/IT111L 11-09-23/IT111L_Game/GameInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT111L_Game
+{
+    internal class GameInfo
+    {
+                
+        private int level = 1;
+
+        public int Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
+
+        private int score = 0;
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+
+    }
+}
---
+++ MiniGame/IT111L 11-09-23/IT111L_Game/Timer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT111L_Game
+{
+    internal class Timer
+    {
+        static public System.Windows.Forms.Timer gameMainTimer = new System.Windows.Forms.Timer();
+        public Player GetPlayer { get { return Level1.player; } }
+
+        public void InitializeComponentLevel()
+        {
+            TimerComponent();
+        }
+
+        public void TimerComponent()
+        {
+            gameMainTimer.Interval = 20;
+            gameMainTimer.Enabled = true;
+
+            gameMainTimer.Tick += GameTimer_Tick;
+
+            gameMainTimer.Start();
+        }
+
+
+        public void GameTimer_Tick(object sender, EventArgs e)
+        {
+            
+            
+            
+            // right window
+            if (GetPlayer.PlayerRight && GetPlayer.PlayerGame.Left < 1135)
+            {
+                GetPlayer.PlayerGame.Left += 5;
+                GetPlayer.PlayerGame.Image = Resources.right;
+            }
+
+            // left window
+            if (GetPlayer.PlayerLeft && GetPlayer.PlayerGame.Left > 0)
+            {
+                GetPlayer.PlayerGame.Left -= 5;
+                GetPlayer.PlayerGame.Image = Resources.left;
+            }
+
+            // top window
+            if (GetPlayer.PlayerUp && GetPlayer.PlayerGame.Top > 65)
+            {
+                GetPlayer.PlayerGame.Top -= 5;
+                GetPlayer.PlayerGame.Image = Resources.back;
+            }
+
+            // down window
+            if (GetPlayer.PlayerDown && GetPlayer.PlayerGame.Top < 710)
+            {
+                GetPlayer.PlayerGame.Top += 5;
+                GetPlayer.player.Image = Resources.front;
+            }
+
+        }
+
+
+    }
+}
