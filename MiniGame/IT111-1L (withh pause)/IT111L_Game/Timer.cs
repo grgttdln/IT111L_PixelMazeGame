7f9887469e14576a0dd47ab87f2d7b5b2a0acbfc
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-11 17:06:16 +0800

    Add files via upload

---
+++ MiniGame/IT111-1L (withh pause)/IT111L_Game/Timer.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Security.Policy;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IT111L_Game
+{
+    internal class Timer
+    {
+        static public System.Windows.Forms.Timer gameMainTimer = new System.Windows.Forms.Timer();
+        public Player GetPlayer { get { return Level1.player; } }
+
+
+        public void InitializeComponentLevel()
+        {
+            TimerComponent();
+        }
+
+        public void TimerComponent()
+        {
+            gameMainTimer.Interval = 20;
+            gameMainTimer.Enabled = true;
+            gameMainTimer.Tick += GameTimer_Tick;
+            gameMainTimer.Start();
+        }
+
+
+        public void StatsDisplay()
+        {
+            Program.gInfo.ScoreDisplay.Text = $"Score: {Program.gInfo.Score}";
+            Program.gInfo.LifeDisplay.Text = $"Life: {Program.gInfo.Life}";
+        }
+
+
+        public void PauseGameTimer()
+        {
+            
+        }
+
+        public void ContPauseGameTimer()
+        {
+            
+        }
+
+
+
+        public void PlayerGameWindowBounds()
+        {
+            // right window
+            if (GetPlayer.PlayerRight && GetPlayer.PlayerGame.Left < 1135)
+            {
+                GetPlayer.PlayerGame.Left += 5;
+                GetPlayer.PlayerGame.Image = Resources.right;
+            }
+
+            // left window
+            if (GetPlayer.PlayerLeft && GetPlayer.PlayerGame.Left > 0)
+            {
+                GetPlayer.PlayerGame.Left -= 5;
+                GetPlayer.PlayerGame.Image = Resources.left;
+            }
+
+            // top window
+            if (GetPlayer.PlayerUp && GetPlayer.PlayerGame.Top > 65)
+            {
+                GetPlayer.PlayerGame.Top -= 5;
+                GetPlayer.PlayerGame.Image = Resources.back;
+            }
+
+            // down window
+            if (GetPlayer.PlayerDown && GetPlayer.PlayerGame.Top < 710)
+            {
+                GetPlayer.PlayerGame.Top += 5;
+                GetPlayer.player.Image = Resources.front;
+            }
+        }
+
+
+        public void GameTimer_Tick(object sender, EventArgs e)
+        {
+
+            //Console.WriteLine(Program.gInfo.Pause);
+
+            if (!Program.gInfo.Pause)
+            {
+                //gameMainTimer.Start();
+                //gameMainTimer.Enabled = true;
+                foreach (Control item in NewGame.level_1.Level1_MainPanel.Controls)
+                {
+                    if (item.Tag == "pause")
+                    {
+                        continue;
+                    }
+                    item.Enabled = true;
+                }
+            }
+            else
+            {
+                //gameMainTimer.Stop();
+                //gameMainTimer.Enabled = false;
+
+                foreach (Control item in NewGame.level_1.Level1_MainPanel.Controls)
+                {
+                    if (item.Tag == "pause")
+                    {
+                        continue;
+                    }
+                    item.Enabled = false; 
+                }
+            }
+
+
+            if (!Program.gInfo.Pause)
+            {
+                // right window
+                if (GetPlayer.PlayerRight && GetPlayer.PlayerGame.Left < 1135)
+                {
+                    GetPlayer.PlayerGame.Left += 5;
+                    GetPlayer.PlayerGame.Image = Resources.right;
+                }
+
+                // left window
+                if (GetPlayer.PlayerLeft && GetPlayer.PlayerGame.Left > 0)
+                {
+                    GetPlayer.PlayerGame.Left -= 5;
+                    GetPlayer.PlayerGame.Image = Resources.left;
+                }
+
+                // top window
+                if (GetPlayer.PlayerUp && GetPlayer.PlayerGame.Top > 65)
+                {
+                    GetPlayer.PlayerGame.Top -= 5;
+                    GetPlayer.PlayerGame.Image = Resources.back;
+                }
+
+                // down window
+                if (GetPlayer.PlayerDown && GetPlayer.PlayerGame.Top < 710)
+                {
+                    GetPlayer.PlayerGame.Top += 5;
+                    GetPlayer.player.Image = Resources.front;
+                }
+
+
+
+                // check
+                foreach (Control item in NewGame.level_1.Level1_MainPanel.Controls)
+                {
+                    if (item is Label)
+                    {
+                        // coin score
+                        if ((string)item.Tag == "coin")
+                        {
+                            if (GetPlayer.PlayerGame.Bounds.IntersectsWith(item.Bounds) && item.Visible == true)
+                            {
+                                Program.gInfo.Score += 1;
+                                //item.Visible = false;
+                                NewGame.level_1.Level1_MainPanel.Controls.Remove(item);
+                            }
+                        }
+                        // enemy
+                        if ((string)item.Name == "enemy")
+                        {
+                            if (GetPlayer.PlayerGame.Bounds.IntersectsWith(item.Bounds))
+                            {
+                                Program.gInfo.Life -= 1;
+                                //GetPlayer.PlayerGame.Image = Resources.right_lose;
+                                GetPlayer.PlayerGame.Left -= 35;
+                                //GetPlayer.PlayerGame.Image = Resources.front_lose;
+                            }
+
+
+                            foreach (Label wallItem in NewGame.level_1.wallsCollection)
+                            {
+                                if (wallItem == null)
+                                {
+                                    continue;
+                                }
+
+                                if ((string)item.Tag == "enemySide")
+                                {
+                                    if (item.Bounds.IntersectsWith(wallItem.Bounds))
+                                    {
+                                        int idx = Array.IndexOf(NewGame.level_1.enemiesCollection, item);
+                                        NewGame.level_1.enemiesSpeedCollection[idx] = -NewGame.level_1.enemiesSpeedCollection[idx];
+                                    }
+                                }
+
+                                if ((string)item.Tag == "enemyUp")
+                                {
+                                    if (item.Bounds.IntersectsWith(wallItem.Bounds))
+                                    {
+                                        int idx = Array.IndexOf(NewGame.level_1.enemiesCollection, item);
+                                        NewGame.level_1.enemiesSpeedCollection[idx] = -NewGame.level_1.enemiesSpeedCollection[idx];
+                                    }
+                                }
+                            }
+                        }
+
+
+                        if ((string)item.Tag == "wall")
+                        {
+                            if (GetPlayer.PlayerGame.Bounds.IntersectsWith(item.Bounds))
+                            {
+                                if (GetPlayer.PlayerLeft)
+                                {
+                                    GetPlayer.PlayerGame.Left += 5;
+                                }
+
+                                if (GetPlayer.PlayerRight)
+                                {
+                                    GetPlayer.PlayerGame.Left -= 5;
+                                }
+
+                                if (GetPlayer.PlayerUp)
+                                {
+                                    GetPlayer.PlayerGame.Top += 5;
+                                }
+
+
+                                if (GetPlayer.PlayerDown)
+                                {
+                                    GetPlayer.PlayerGame.Top -= 5;
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+
+
+                // enemy movement
+
+                // enemies
+
+                for (int i = 0; i < 2; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        NewGame.level_1.enemiesCollection[i].Left += NewGame.level_1.enemiesSpeedCollection[i];
+                    }
+                    else
+                    {
+                        NewGame.level_1.enemiesCollection[i].Top += NewGame.level_1.enemiesSpeedCollection[i];
+                    }
+                }
+
+                //NewGame.level_1.enemiesCollection[0].Left += NewGame.level_1.enemiesSpeedCollection[0];
+                //NewGame.level_1.enemiesCollection[1].Left += NewGame.level_1.enemiesSpeedCollection[1];
+            }
+
+            StatsDisplay();
+        }
+
+
+        //StatsDisplay();
+        //PlayerGameWindowBounds();
+
+        /*
+        // right window
+        if (GetPlayer.PlayerRight && GetPlayer.PlayerGame.Left < 1135)
+        {
+            GetPlayer.PlayerGame.Left += 5;
+            GetPlayer.PlayerGame.Image = Resources.right;
+        }
+
+        // left window
+        if (GetPlayer.PlayerLeft && GetPlayer.PlayerGame.Left > 0)
+        {
+            GetPlayer.PlayerGame.Left -= 5;
+            GetPlayer.PlayerGame.Image = Resources.left;
+        }
+
+        // top window
+        if (GetPlayer.PlayerUp && GetPlayer.PlayerGame.Top > 65)
+        {
+            GetPlayer.PlayerGame.Top -= 5;
+            GetPlayer.PlayerGame.Image = Resources.back;
+        }
+
+        // down window
+        if (GetPlayer.PlayerDown && GetPlayer.PlayerGame.Top < 710)
+        {
+            GetPlayer.PlayerGame.Top += 5;
+            GetPlayer.player.Image = Resources.front;
+        }
+
+
+
+        // check
+        foreach (Control item in NewGame.level_1.Level1_MainPanel.Controls)
+        {
+            if (item is Label)
+            {
+                // coin score
+                if ((string)item.Tag == "coin")
+                {
+                    if (GetPlayer.PlayerGame.Bounds.IntersectsWith(item.Bounds) && item.Visible == true)
+                    {
+                        Program.gInfo.Score += 1;
+                        //item.Visible = false;
+                        NewGame.level_1.Level1_MainPanel.Controls.Remove(item);
+                    }
+                }
+                // enemy
+                if ((string)item.Name == "enemy")
+                {
+                    if (GetPlayer.PlayerGame.Bounds.IntersectsWith(item.Bounds))
+                    {
+                        Program.gInfo.Life -= 1;
+                        //GetPlayer.PlayerGame.Image = Resources.right_lose;
+                        GetPlayer.PlayerGame.Left -= 35;
+                        //GetPlayer.PlayerGame.Image = Resources.front_lose;
+                    }
+
+
+                    foreach (Label wallItem in NewGame.level_1.wallsCollection)
+                    {
+                        if (wallItem == null)
+                        {
+                            continue;
+                        }
+
+                        if ((string)item.Tag == "enemySide")
+                        {
+                            if (item.Bounds.IntersectsWith(wallItem.Bounds))
+                            {
+                                int idx = Array.IndexOf(NewGame.level_1.enemiesCollection, item);
+                                NewGame.level_1.enemiesSpeedCollection[idx] = -NewGame.level_1.enemiesSpeedCollection[idx];
+                            }
+                        }
+
+                        if ((string)item.Tag == "enemyUp")
+                        {
+                            if (item.Bounds.IntersectsWith(wallItem.Bounds))
+                            {
+                                int idx = Array.IndexOf(NewGame.level_1.enemiesCollection, item);
+                                NewGame.level_1.enemiesSpeedCollection[idx] = -NewGame.level_1.enemiesSpeedCollection[idx];
+                            }
+                        }
+                    }
+                }
+
+
+                if ((string)item.Tag == "wall")
+                {
+                    if (GetPlayer.PlayerGame.Bounds.IntersectsWith(item.Bounds))
+                    {
+                        if (GetPlayer.PlayerLeft)
+                        {
+                            GetPlayer.PlayerGame.Left += 5;
+                        }
+
+                        if (GetPlayer.PlayerRight)
+                        {
+                            GetPlayer.PlayerGame.Left -= 5;
+                        }
+
+                        if (GetPlayer.PlayerUp)
+                        {
+                            GetPlayer.PlayerGame.Top += 5;
+                        }
+
+
+                        if (GetPlayer.PlayerDown)
+                        {
+                            GetPlayer.PlayerGame.Top -= 5;
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+
+        // enemy movement
+
+        // enemies
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (i % 2 == 0)
+            {
+                NewGame.level_1.enemiesCollection[i].Left += NewGame.level_1.enemiesSpeedCollection[i];
+            }
+            else
+            {
+                NewGame.level_1.enemiesCollection[i].Top += NewGame.level_1.enemiesSpeedCollection[i];
+            }
+        }
+
+            //NewGame.level_1.enemiesCollection[0].Left += NewGame.level_1.enemiesSpeedCollection[0];
+            //NewGame.level_1.enemiesCollection[1].Left += NewGame.level_1.enemiesSpeedCollection[1];
+    }
+        */
+    }
+}
+
+
