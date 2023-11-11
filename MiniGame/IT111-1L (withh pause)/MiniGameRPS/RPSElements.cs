490c30d3bcb61a56e542333899018bf7acd14973
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-11 17:09:33 +0800

    Add files via upload

---
+++ MiniGame/IT111-1L (withh pause)/MiniGameRPS/RPSElements.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MiniGameRPS
+{
+    internal class RPSElements
+    {
+        public static FontGameCollection.FontGame fontGame = new FontGameCollection.FontGame();
+        public RPSLogic rpsLogic;
+        public RPSForm rpsForm;
+
+        public RPSElements(RPSForm form)
+        {
+            rpsForm = form;
+            rpsLogic = new RPSLogic(this, form);
+            rockButton.Click += rpsLogic.GetChoice_Click;
+            paperButton.Click += rpsLogic.GetChoice_Click;
+            scissorButton.Click += rpsLogic.GetChoice_Click;
+
+
+        }
+
+        private Label cardMiniTitle = new Label
+        {
+            Text = "Mini Game Trap: RPS Royale",
+            Size = new Size(800, 50),
+            Font = new Font(fontGame.pfc.Families[1], 28),
+            Location = new Point(-6, 5),
+            ForeColor = Color.White,
+            BackColor = Color.Transparent,
+            TextAlign = ContentAlignment.MiddleCenter
+        };
+
+        public Label CardMiniTitle
+        {
+            get { return cardMiniTitle; }
+            set { cardMiniTitle = value; }
+
+        }
+
+        private Label sdText = new Label()
+        {
+            Text = "Swift choices, instant win – defeat your opponent in a single move!",
+            Size = new Size(800, 50),
+            Font = new Font(fontGame.pfc.Families[0], 16),
+            Location = new Point(-6, 50),
+            ForeColor = Color.White,
+            BackColor = Color.Transparent,
+            TextAlign = ContentAlignment.MiddleCenter
+        };
+
+        public Label SDText
+        {
+            get { return sdText; }
+            set { sdText = value; }
+
+        }
+
+
+        private PictureBox playerRPS = new PictureBox()
+        {
+            Height = 250,
+            Width = 250,
+            BackColor = Color.White,
+            Image = RPSResources._default,
+            SizeMode = PictureBoxSizeMode.StretchImage,
+            Location = new Point(115, 100)
+        };
+
+
+        public PictureBox PlayerRPS
+        {
+            get { return playerRPS; }
+            set { playerRPS = value; }
+        }
+
+        private PictureBox compRPS = new PictureBox()
+        {
+            Height = 250,
+            Width = 250,
+            BackColor = Color.White,
+            Image = RPSResources._default,
+            SizeMode = PictureBoxSizeMode.StretchImage,
+            Location = new Point(425, 100)
+        };
+
+        public PictureBox CompRPS
+        {
+            get { return compRPS; }
+            set { compRPS = value; }
+        }
+
+
+        private Button rockButton = new Button()
+        {
+            Text = "ROCK",
+            Font = new Font(fontGame.pfc.Families[0], 18),
+            ForeColor = Color.Black,
+            BackgroundImage = RPSResources.rockButton,
+            BackgroundImageLayout = ImageLayout.Stretch,
+            BackColor = Color.Transparent,
+            Location = new Point((490 - 120) / 2, 360),
+            Size = new Size(140, 80),
+            FlatStyle = FlatStyle.Flat,
+            FlatAppearance = { BorderSize = 0, BorderColor = Color.FromArgb(0, 255, 255, 255), MouseOverBackColor = Color.Transparent, MouseDownBackColor = Color.Transparent },
+        };
+
+        public Button RockButton
+        {
+            get { return rockButton; }
+            set { rockButton = value; }
+        }
+
+
+        private Button paperButton = new Button()
+        {
+            Text = "PAPER",
+            Font = new Font(fontGame.pfc.Families[0], 18),
+            ForeColor = Color.Black,
+            BackgroundImage = RPSResources.paperButton,
+            BackgroundImageLayout = ImageLayout.Stretch,
+            BackColor = Color.Transparent,
+            Location = new Point((770 - 120) / 2, 360),
+            Size = new Size(140, 80),
+            FlatStyle = FlatStyle.Flat,
+            FlatAppearance = { BorderSize = 0, BorderColor = Color.FromArgb(0, 255, 255, 255), MouseOverBackColor = Color.Transparent, MouseDownBackColor = Color.Transparent },
+        };
+
+        public Button PaperButton
+        {
+            get { return paperButton; }
+            set { paperButton = value; }
+        }
+
+        private Button scissorButton = new Button()
+        {
+            Text = "SCISSORS",
+            Font = new Font(fontGame.pfc.Families[0], 18),
+            ForeColor = Color.Black,
+            BackgroundImage = RPSResources.scissorsButton,
+            BackgroundImageLayout = ImageLayout.Stretch,
+            BackColor = Color.Transparent,
+            Location = new Point((1050 - 120) / 2, 360),
+            Size = new Size(140, 80),
+            FlatStyle = FlatStyle.Flat,
+            FlatAppearance = { BorderSize = 0, BorderColor = Color.FromArgb(0, 255, 255, 255), MouseOverBackColor = Color.Transparent, MouseDownBackColor = Color.Transparent },
+        };
+
+        public Button ScissorButton
+        {
+            get { return scissorButton; }
+            set { scissorButton = value; }
+        }
+    }
+}
---
+++ MiniGame/IT111-1L (withh pause)/MiniGameRiddles/RiddleForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using FontGameCollection;
+
+namespace MiniGameRiddles
+{
+    internal class RiddleForm : Form
+    {
+        public RiddleElements elements;
+
+        public RiddleLogic rLogic;
+
+        public static int currentLevel;
+
+        public MiniGameTimer timer = new MiniGameTimer();
+
+        public RiddleForm(int level)
+        {
+            currentLevel = level;
+
+
+            this.Size = new Size(800, 500);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Text = "Pixel Quest Mini Game: Riddles";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackgroundImage = RiddleResources.minigame_bg;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            elements = new RiddleElements(this);
+            rLogic = new RiddleLogic(this, elements);
+
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.ControlBox = false;
+            AddElements();
+
+
+        }
+
+        public void AddElements()
+        {
+            this.Controls.Add(elements.CardMiniTitle);
+            this.Controls.Add(elements.QuestionLabel);
+            this.Controls.Add(elements.answerTextBox);
+            this.Controls.Add(elements.submitButton);
+            this.Controls.Add(timer.time);
+        }
+
+        private static RiddleForm instance;
+
+        public static RiddleForm Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new RiddleForm(3);
+                }
+                return instance;
+            }
+        }
+
+
+        public void CloseForm()
+        {
+            Application.ExitThread();
+        }
+    }
+}
