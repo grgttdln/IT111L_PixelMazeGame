b209d84ac370d94331e42ee7f46c7404c9b116f2
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-13 21:14:49 +0800

    Add files via upload

---
+++ MiniGame/11-13-23 (TIMER TIMER)/FontGameCollection/FontGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace FontGameCollection
+{
+    public class FontGame
+    {
+        public PrivateFontCollection pfc = new PrivateFontCollection();
+
+        public FontGame()
+        {
+
+            string executableDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string fontFilePath_0 = Path.Combine(executableDirectory, "assets/fonts", "Kenney_Pixel.ttf");
+            string fontFilePath_1 = Path.Combine(executableDirectory, "assets/fonts", "Kenney_Pixel_Square.ttf");
+
+            pfc.AddFontFile(fontFilePath_1);
+            pfc.AddFontFile(fontFilePath_0); 
+        }
+    }
+}
