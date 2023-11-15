f99a0ef2d544e9dff02c382c1d243539a9a77bd9
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-15 19:16:09 +0800

    Add files via upload

---
+++ MiniGame/11-15-23 (1)/MiniGameWindow/MiniGameForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MiniGameWindow
+{
+    public class MiniGameForm
+    {
+        public int Level { get; set; }
+        public bool IsWin { get; set; }
+        public MiniGameForm(int lvl)
+        {
+            Level = lvl;
+            IsWin = false;
+        }
+
+        public virtual void MiniGameMainDisplay()
+        {
+            
+        }
+    }
+}
