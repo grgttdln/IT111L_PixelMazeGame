fd55e14745a4105e9ae90904c00f28d6402db914
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-11-17 11:56:51 +0800

    Add files via upload

---
+++ MiniGame/11-17-20/MiniGameCardMemory/CardGameLogic.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Resources;
+using System.Media;
+
+namespace MiniGameCardMemory
+{
+
+    internal class CardGameLogic
+    {
+        MiniGameMainTimer gameTimer = new MiniGameMainTimer();
+
+
+        private SoundPlayer soundPlayer = new SoundPlayer();
+
+
+        public string firstCard;
+        public string secondCard;
+        public PictureBox cardA = new PictureBox();
+        public PictureBox cardB = new PictureBox();
+
+        public int counter = 0;
+
+        public void IsWinner(int row)
+        {
+            if (counter == row)
+            {
+                foreach (PictureBox x in CardGameInfo.cardPicturesCollection)
+                {
+                    if (x.Tag != null)
+                    {
+                        x.Image = (Image)MiniGameCardResources.ResourceManager.GetObject("_" + x.Tag.ToString());
+                    }
+                }
+                soundPlayer.Stream = MiniGameCardResources.win;
+                soundPlayer.Play();
+                gameTimer.CGFormTimerStop("You Won!");
+                return;
+            }
+        }
+
+        public void CardClicked(object sender, EventArgs e)
+        {
+            soundPlayer.Stream = MiniGameCardResources.flipcard;
+            soundPlayer.Play();
+
+            if (CardGameInfo.gameOver)
+            {
+                return;
+            }
+
+            switch (CardGameForm.currentPlayerLevel)
+            {
+                case 1:
+                    IsWinner(3);
+                    break;
+                case 2:
+                    IsWinner(4);
+                    break;
+                case 3:
+                    IsWinner(5);
+                    break;
+            }
+
+            CheckCards(cardA, cardB);
+
+            if (firstCard == null || secondCard == null)
+            {
+                if (firstCard == null)
+                {
+                    cardA = sender as PictureBox;
+
+                    if (cardA.Tag != null)
+                    {
+                        cardA.Image = (Image)MiniGameCardResources.ResourceManager.GetObject("_" + cardA.Tag.ToString());
+                        firstCard = (string)cardA.Tag;
+                    }
+                }
+                else if (secondCard == null)
+                {
+                    cardB = sender as PictureBox;
+
+                    if (cardB != cardA && cardB.Tag != null)
+                    {
+                        cardB.Image = (Image)MiniGameCardResources.ResourceManager.GetObject("_" + cardB.Tag.ToString());
+                        secondCard = (string)cardB.Tag;
+                    }
+
+                }
+
+            }
+
+        }
+
+
+        public void FlipCards()
+        {
+            foreach (PictureBox p in CardGameInfo.cardPicturesCollection.ToList())
+            {
+                if (p.Tag != null)
+                {
+                    p.Image = (Image)MiniGameCardResources.ResourceManager.GetObject("card_back");
+                }
+            }
+        }
+
+        public void CheckCards(PictureBox A, PictureBox B)
+        {
+            try
+            {
+                if (firstCard != null && secondCard != null)
+                {
+                    if (firstCard == secondCard)
+                    {
+                        A.Tag = null;
+                        B.Tag = null;
+                        counter += 1;
+                        CardGameInfo.totalCards -= 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mismatched");
+                    }
+
+                    firstCard = null;
+                    secondCard = null;
+
+                    FlipCards();
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+        public void NewGameOrderCards()
+        {
+            gameTimer.CGFormTimerStart();
+        }
+    }
+}
---
+++ MiniGame/11-17-20/MiniGameLogicQuiz/MiniGameMainTimer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Media;
+
+
+namespace MiniGameLogicQuiz
+{
+    internal class MiniGameMainTimer : QuizGameInfo
+    {
+        public static System.Windows.Forms.Timer miniGameTick = new System.Windows.Forms.Timer();
+
+        private SoundPlayer soundPlayer = new SoundPlayer();
+        public Label time = QuizGameInfo.LblTimer;
+        public Label questionssLeft = QuizGameInfo.LblRightAnsLeft;
+
+        public bool isFinish = false;
+
+        public static Timer GetTimer
+        {
+            get { return miniGameTick; }
+            set { miniGameTick = value; }
+        }
+
+        public void CGFormTimerStart()
+        {
+            miniGameTick.Interval = 1000;
+            miniGameTick.Enabled = true;
+            miniGameTick.Tick -= GameActions;
+            miniGameTick.Tick += GameActions;
+            miniGameTick.Start();
+        }
+
+        public void CGFormTimerStop(string message)
+        {
+            isGameOver = true;
+
+            if (isFinish)
+            {
+                miniGameTick.Stop();
+                MessageBox.Show(message);
+            }
+
+
+            if (!isFinish)
+            {
+                soundPlayer.Stream = MiniGameQuizResources.lose;
+                soundPlayer.Play();
+                isFinish = true;
+                miniGameTick.Stop();
+                MessageBox.Show(message);
+
+                    
+                
+            }
+            //// else if -> if
+            else if (!isFinish && message == "You Lose!")
+            {
+                soundPlayer.Stream = MiniGameQuizResources.lose;
+                soundPlayer.Play();
+                isFinish = true;
+                isGameOver = true;
+                miniGameTick.Stop();
+                MessageBox.Show(message);
+                
+            }
+
+            QuizGameForm.form.CloseForm();
+        }
+
+        public void GameFormTimer()
+        {
+            totalTime--;
+            LblTimer.Text = $"Time Left: {totalTime}";
+            LblRightAnsLeft.Text = $"Questions Left to Answer: {currScore}";
+        }
+
+        public void GameActions(object sender, EventArgs e)
+        {
+            if (!isFinish)
+            {
+                Console.WriteLine(totalTime);
+                GameFormTimer();
+
+                if (totalTime == 0 && !isFinish)
+                {
+                    
+                    CGFormTimerStop("You Lose!");
+                }
+            }
+        }
+    }
+}
