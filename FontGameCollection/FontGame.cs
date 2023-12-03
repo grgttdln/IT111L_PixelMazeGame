b7c9024fdc40d7e3fcb0ebe816a80eb6f0480fab
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-12-03 17:19:25 +0800

    update game final version

---
+++ FontGameCollection/FontGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace FontGameCollection
+{
+    public class FontGame
+    {
+        // PrivateFontCollection instance to manage custom fonts.
+        public PrivateFontCollection pfc = new PrivateFontCollection();
+
+        // Constructor for the FontGame class.
+        public FontGame()
+        {
+            // Get the directory where the executable is located.
+            string executableDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // Define the file paths for two custom fonts in the "assets/fonts" directory.
+            string fontFilePath_0 = Path.Combine(executableDirectory, "assets/fonts", "Kenney_Pixel.ttf");
+            string fontFilePath_1 = Path.Combine(executableDirectory, "assets/fonts", "Kenney_Pixel_Square.ttf");
+
+            // Add the first custom font file to the PrivateFontCollection.
+            pfc.AddFontFile(fontFilePath_1);
+
+            // Add the second custom font file to the PrivateFontCollection.
+            pfc.AddFontFile(fontFilePath_0);
+        }
+    }
+}
---
+++ GameFileHandler/MGReadQuestion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// The GameFileHandler namespace encapsulates classes related to handling game files.
+
+namespace GameFileHandler
+{
+    // The MGReadQuestion class provides methods for reading questions from a text file.
+    public class MGReadQuestion
+    {
+        // Reads a text file containing questions and returns an array of strings representing each question.
+        // Parameters:
+        //   filename - The path to the text file containing questions.
+        // Returns:
+        //   An array of strings representing each question in the text file, or null if an exception occurs.
+        public string[] ReadTxtFileQuestion(string filename)
+        {
+            // Initialize the array to store the questions.
+            string[] question = null;
+
+            try
+            {
+                // Attempt to read all lines from the specified text file.
+                question = File.ReadAllLines(filename);
+            }
+            catch (Exception e)
+            {
+                // If an exception occurs during file reading, set the question array to null.
+                question = null;
+            }
+
+            // Return the array of questions.
+            return question;
+        }
+    }
+}
